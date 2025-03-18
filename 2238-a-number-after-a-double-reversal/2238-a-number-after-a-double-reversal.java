2847ec1e2482c7c4a26f23446d064979c8bc6180
vijayvk10 <126579351+vijayvk10@users.noreply.github.com>
2025-03-18 18:48:08 +0530

    Time: 0 ms (100%), Space: 40.6 MB (33.72%) - LeetHub

---
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if(num%10==0 && num != 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+}
